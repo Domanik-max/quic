c418c6330db5b157a453901abd48f16e16c694cb
Your Name <you@example.com>
2019-12-09 19:40:04 -0600

    completed reg and login page and working on questions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {RegisterComponent} from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import { QuestionComponent } from './question/question.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',           component: RegisterComponent},
+  {path: 'register',   component: RegisterComponent},
+  {path: 'login',      component: LoginComponent},
+  {path: 'question',   component: QuestionComponent},
+  {path: '**',         component: PagenotfoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from './material/material.module';
+import { LoginComponent } from './login/login.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { QuestionComponent } from './question/question.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    PagenotfoundComponent,
+    NavbarComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
